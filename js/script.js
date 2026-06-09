da1218b7a2899b2d42c92b80069b26371f0157cc
KevinMatt21 <juanlopez74937@gmail.com>
2026-06-09 10:30:39 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,3 +35,57 @@ setInterval(() => {
   document.getElementById("minutes").innerText = minutes;
   document.getElementById("seconds").innerText = seconds;
 }, 1000);
+// =====================
+// PETALOS
+// =====================
+
+function createPetal(){
+
+    const petal = document.createElement("div");
+
+    petal.classList.add("petal");
+
+    petal.innerHTML = "🌸";
+
+    petal.style.left = Math.random()*100 + "vw";
+
+    petal.style.animationDuration =
+    (5 + Math.random()*8) + "s";
+
+    petal.style.opacity =
+    0.4 + Math.random()*0.6;
+
+    document.body.appendChild(petal);
+
+    setTimeout(()=>{
+        petal.remove();
+    },13000);
+}
+
+setInterval(createPetal,700);
+
+// =====================
+// PARTICULAS DORADAS
+// =====================
+
+const particlesContainer =
+document.getElementById("particles-container");
+
+for(let i=0;i<60;i++){
+
+    const particle =
+    document.createElement("div");
+
+    particle.classList.add("particle");
+
+    particle.style.left =
+    Math.random()*100 + "%";
+
+    particle.style.top =
+    Math.random()*100 + "%";
+
+    particle.style.animationDelay =
+    Math.random()*6 + "s";
+
+    particlesContainer.appendChild(particle);
+}
